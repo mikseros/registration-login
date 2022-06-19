2e6c989d1894b729558abbd444fb45e8ebbc91a3
mikseros <mmadynski@gmail.com>
2022-06-19 21:52:57 +0200

    Test createUser()

---
+++ src/test/java/com/mikseros/registrationlogin/UserRepositoryTests.java
@@ -0,0 +1,39 @@
+package com.mikseros.registrationlogin;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import com.mikseros.registrationlogin.model.User;
+import com.mikseros.registrationlogin.repository.UserRepository;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	@Autowired
+	private TestEntityManager entityManager;
+	
+	@Test
+	public void testCreateUser() {
+		User user = new User();
+		user.setEmail("xxx@xxx.com");
+		user.setFirstName("Xxx");
+		user.setLastName("XXX");
+		user.setPassword("12345");
+		
+		User savedUser = repo.save(user);
+		User existUser = entityManager.find(User.class, savedUser.getId());
+		assertThat(existUser.getEmail()).isEqualTo(user.getEmail());
+	}
+}
