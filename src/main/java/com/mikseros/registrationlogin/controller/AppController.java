2060e7abf1abf4e6e094dab8a4dfc732d43878b6
mikseros <mmadynski@gmail.com>
2022-06-20 20:49:23 +0200

    User Registration -> end-point in the Controller

--- src/main/java/com/mikseros/registrationlogin/controller/AppController.java
+++ src/main/java/com/mikseros/registrationlogin/controller/AppController.java
@@ -1,7 +1,10 @@
 package com.mikseros.registrationlogin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.mikseros.registrationlogin.model.User;
 
 @Controller
 public class AppController {
@@ -9,5 +12,12 @@ public class AppController {
 	@GetMapping("")
 	public String viewHomePage() {
 		return "index";
+	}
+	
+	@GetMapping("/register")
+	public String showSignUpForm(Model model) {
+		model.addAttribute("user", new User());
+		
+		return "signup_form";
 	}
 }
