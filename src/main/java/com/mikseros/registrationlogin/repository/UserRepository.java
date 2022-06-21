26ff57fa220d3d7059b610f7307a2046160a8e95
mikseros <mmadynski@gmail.com>
2022-06-21 14:43:05 +0200

    New function in UserRepository (findByEmail)

--- src/main/java/com/mikseros/registrationlogin/repository/UserRepository.java
+++ src/main/java/com/mikseros/registrationlogin/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.mikseros.registrationlogin.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.mikseros.registrationlogin.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	
+	@Query("SELECT u FROM User u WHERE u.email = ?1")
+	User findByEmail(String email);
 }
