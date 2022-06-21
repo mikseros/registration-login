90544c11037d8eb7b295d8bd67c755ea2eed2a7d
mikseros <mmadynski@gmail.com>
2022-06-21 15:06:23 +0200

    LoadUserByUserName

--- src/main/java/com/mikseros/registrationlogin/security/CustomUserDetailsService.java
+++ src/main/java/com/mikseros/registrationlogin/security/CustomUserDetailsService.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import com.mikseros.registrationlogin.model.User;
 import com.mikseros.registrationlogin.repository.UserRepository;
 
 public class CustomUserDetailsService implements UserDetailsService {
@@ -14,8 +15,11 @@ public class CustomUserDetailsService implements UserDetailsService {
 	
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		User user = repo.findByEmail(email);
+		if (user == null) {
+			throw new UsernameNotFoundException("User not found");
+		}
+		return new CustomUserDetails(user);
 	}
 
 }
