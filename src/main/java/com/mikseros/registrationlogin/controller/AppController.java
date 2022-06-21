e9608a16f5516a71d023f6d7153aebb9f7e5e8e7
mikseros <mmadynski@gmail.com>
2022-06-21 13:26:58 +0200

    Register User -> Save in the Controller

--- src/main/java/com/mikseros/registrationlogin/controller/AppController.java
+++ src/main/java/com/mikseros/registrationlogin/controller/AppController.java
@@ -1,14 +1,20 @@
 package com.mikseros.registrationlogin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.mikseros.registrationlogin.model.User;
+import com.mikseros.registrationlogin.repository.UserRepository;
 
 @Controller
 public class AppController {
 
+	@Autowired
+	private UserRepository repo;
+	
 	@GetMapping("")
 	public String viewHomePage() {
 		return "index";
@@ -19,5 +25,11 @@ public class AppController {
 		model.addAttribute("user", new User());
 		
 		return "signup_form";
+	}
+	
+	@PostMapping("/process_register")
+	public String processRegistration(User user) {
+		repo.save(user);
+		return "register_success";
 	}
 }
